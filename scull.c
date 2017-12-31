b25a65127b2d5469575389769f51175aa2d208f8
Florent Flament <contact@florentflament.com>
2017-12-31 10:25:23 +0100

    Cleanup - Add test to copy_(from/to)_user calls

--- scull.c
+++ scull.c
@@ -1,4 +1,5 @@
 #include <linux/cdev.h>
+#include <linux/errno.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
@@ -8,14 +9,20 @@
 MODULE_LICENSE("GPL v2");
 
 static dev_t scull_dev;
-static struct cdev *scull_cdev;
+static struct cdev scull_cdev;
 
 static ssize_t scull_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
 {
+  // Capping count to length of SCULL_MSG
   #define SCULL_MSG "Hello !\n"
   #define SCULL_MLEN 8
   count = count < SCULL_MLEN ? count : SCULL_MLEN;
-  count -= copy_to_user(buf, SCULL_MSG, count);
+
+  if (copy_to_user(buf, SCULL_MSG, count)) {
+    printk(KERN_ERR "scull: Failed to copy data to user\n");
+    return -EFAULT;
+  }
+
   printk(KERN_INFO "scull: Sent %lu bytes to user\n", count);
   return count;
 }
@@ -24,8 +31,13 @@ static ssize_t scull_write(struct file *filp, const char __user *buf, size_t cou
 {
   char lbuf[64];
   count = count < 63 ? count : 63; // Read up to 63 chars into the 64 chars buffer
-  count -= copy_from_user(&lbuf, buf, count);
+
+  if (copy_from_user(&lbuf, buf, count)) {
+    printk(KERN_ERR "scull: Failed to copy data from user\n");
+    return -EFAULT;
+  }
   lbuf[count] = '\0';
+
   printk(KERN_INFO "scull: Read %lu bytes from user\n", count);
   printk(KERN_INFO "scull: %s\n", lbuf);
   return count;
@@ -45,29 +57,34 @@ static int scull_init(void)
   result = alloc_chrdev_region(&scull_dev, 0, 1, "scull");
   if (result < 0) {
     printk(KERN_ERR "scull: Failed to register char device number\n");
-    return result;
+    goto out;
   }
   printk(KERN_INFO "scull: Char device number registered: %d %d\n", MAJOR(scull_dev), 0);
 
   // Allocating & registering cdev structure
-  scull_cdev = cdev_alloc();
-  scull_cdev->ops = &scull_fops;
-  scull_cdev->owner = THIS_MODULE;
-  result = cdev_add(scull_cdev, scull_dev, 1) < 0;
+  cdev_init(&scull_cdev, &scull_fops);
+  scull_cdev.owner = THIS_MODULE;
+
+  result = cdev_add(&scull_cdev, scull_dev, 1) < 0;
   if (result < 0) {
     printk(KERN_ERR "scull: Failed to add char device to system\n");
-    unregister_chrdev_region(scull_dev, 1);
-    return result;
+    goto error_cdev;
   }
   printk(KERN_INFO "scull: Char device successfuly added to system\n");
 
-  return 0;
+out:
+  return result;
+
+error_cdev:
+  unregister_chrdev_region(scull_dev, 1);
+  goto out;
 }
 
 static void scull_exit(void)
 {
-  cdev_del(scull_cdev);
+  cdev_del(&scull_cdev);
   printk(KERN_INFO "scull: Char device removed from system\n");
+
   unregister_chrdev_region(scull_dev, 1);
   printk(KERN_INFO "scull: Char device number unregistered: %d %d\n", MAJOR(scull_dev), 0);
   printk(KERN_INFO "scull: Bye !\n");
